bcfaa1f4e9788524d54523dd5a6b57eb7577f551
Afolabi Opeyemi Mariam <opeyemi.afolabi45@gmail.com>
2022-11-24 22:39:48 +0000

    ds

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const nemo = ['nemo'];
+const everyone = ['dory', 'bruce','marlin','gill','bloat','nigel','squirt','darla','hank','nemo'];
+const large = new Array(10000).fill('nemo');
+
+function findNemo(array){
+    for (let i = 0; i < array.length; i++) {
+        console.log('searching')
+      if (array[i] === 'nemo') {
+            console.log('Found NEMO');
+            break;
+      }
+        
+    }
+}
+
+findNemo(everyone); 0(1)
